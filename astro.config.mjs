f32ab79a292aba923cfcbcedb602d7e4f078602c
Seba9989 <sgrzybbowski@gmail.com>
2024-01-10 20:37:29 +0100

    Init Portfolio repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  // vite: {
+  //   css: {
+  //     preprocessorOptions: {
+  //       scss: {
+  //         additionalData: ` 
+  //           @use "astro-scroll-observer/scss" as *;
+  //         `,
+  //       },
+  //     },
+  //   },
+  // },
+});
