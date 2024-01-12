3b64f99087e1165d6e508f4628b70473d62cf3eb
Seba9989 <sgrzybbowski@gmail.com>
2024-01-12 20:56:28 +0100

    Fix all

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://seba9989.github.io",
-  base: "/Portfolio-Website",
+  site: "https://seba9989.com",
 });
