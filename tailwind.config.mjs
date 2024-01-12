defb88d33d50064dc47d224f4aedb219641cb439
Seba9989 <sgrzybbowski@gmail.com>
2024-01-12 21:01:13 +0100

    Small fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,14 @@
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+
+  daisyui: {
+    
+  },
+};
